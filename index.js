a419a9f2b11303db842f1290c922a7970c9df43d
Your Name <you@example.com>
2023-05-04 15:00:06 +0300

    added dotenv

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("ahmed");
+import express from "express";
+import { engine } from 'express-handlebars';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const mongoConectionUrl = 'mongodb://localhost:27017/project';
+
+mongoose.connect(process.env.mongoConectionUrl)
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+const port = 5000;
+
+app.listen(process.env.port, () => {
+    console.log(`started on http://localhost:${process.env.port}`);
+});
